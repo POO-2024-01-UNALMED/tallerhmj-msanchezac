bdf51a7541148e114720513403add97fa1b4c050
msanchezac <msanchezac@unal.edu.co>
2024-08-20 17:15:11 -0500

    Commit 1

---
+++ src/main/java/futbol/Futbolista.java
@@ -0,0 +1,51 @@
+package futbol;
+
+public abstract class Futbolista implements Comparable{
+	private String nombre;
+	private int edad;
+	private final String posicion;
+	
+	public Futbolista() {
+		this.nombre = "Maradona";
+		this.posicion = "delantero";
+		this.edad = 30;
+	}
+	
+	public Futbolista(String nombre, int edad, String posicion) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.posicion = posicion;
+	}
+	
+	public boolean equals(Futbolista p) {
+		return this == p;
+	}
+	
+	abstract public boolean jugarConLasManos();
+	
+	@Override
+	public String toString() {
+		return "El futbolista "+ nombre +"tiene"+ edad +", y juega de"+ posicion;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	public String getPosicion() {
+		return posicion;
+	}
+	
+}
---
+++ src/main/java/futbol/Jugador.java
@@ -0,0 +1,51 @@
+package futbol;
+import java.lang.Math;
+
+public class Jugador extends Futbolista {
+	
+	public short golesMarcados;
+	public byte dorsal;
+	
+	public Jugador() {
+		this.golesMarcados = 289;
+		this.dorsal = 7;
+	}
+
+	public Jugador(String nombre, int edad, String posicion, short golesMarcados, byte dorsal) {
+		super(nombre, edad, posicion);
+		this.golesMarcados = golesMarcados;
+		this.dorsal = dorsal;
+	}
+	
+	@Override
+	public boolean jugarConLasManos() {
+		return false;
+	}
+	
+	@Override
+	public int compareTo(Object f) {
+		Jugador p = (Jugador) f;
+		return Math.abs(this.getEdad()-p.getEdad());	
+	}
+	
+	@Override 
+	public String toString() {
+		return "El futbolista " +getNombre()+ " tiene " +getEdad()+ ", y juega de " +getPosicion()+ " con el dorsal " +dorsal+ ". Ha marcado " +golesMarcados;
+	}
+	
+	public short getGolesMarcados() {
+		return golesMarcados;
+	}
+	
+	public void setGolesMarcados(short golesMarcados) {
+		this.golesMarcados = golesMarcados;
+	}
+	
+	public byte getDorsal() {
+		return dorsal;
+	}
+	
+	public void setDorsal(byte dorsal) {
+		this.dorsal = dorsal;
+	}
+}
